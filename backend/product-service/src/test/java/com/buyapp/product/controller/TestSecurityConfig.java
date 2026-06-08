c1676751fda0ca422e1f8fe0c82ec9bb728dfc50
AliHJMM <AliHasanJMM@gmail.com>
2026-06-08 18:26:34 +0300

    Debugging

--- backend/product-service/src/test/java/com/buyapp/product/controller/TestSecurityConfig.java
+++ backend/product-service/src/test/java/com/buyapp/product/controller/TestSecurityConfig.java
@@ -6,6 +6,7 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.annotation.Order;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -36,6 +37,7 @@ public class TestSecurityConfig {
     }
 
     @Bean
+    @Order(1)
     public SecurityFilterChain testSecurityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
